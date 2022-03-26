7a22325400159a0e35d22ce1785e1780014b7b4d
Elena Borisova <elenaborrisova@gmail.com>
2022-03-26 18:08:06 +0100

    modify file upload

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from datetime import timedelta
 from flask import Flask, render_template, request, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename, redirect
-# import cx_Oracle
+import cx_Oracle
 
 import kpi_data
-from helpers import allowed_file, modify_uploaded_file
+from helpers import allowed_file, modify_uploaded_file, upload_to_db
 
 # Configuration
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
@@ -17,15 +17,13 @@ app.permanent_session_lifetime = timedelta(minutes=10)
 UPLOAD_FOLDER = './file_uploads'
 ALLOWED_EXTENSIONS = {'xlsx', 'xlsm', 'xltx', 'xltm', 'xml'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
-
-# # Oracle Client config
-# os.environ['TNS_ADMIN'] = '/Users/elenaborisova/Documents/GitHub/iberia-solution/wallet'
-# lib_dir = os.path.join(os.environ.get('HOME'), 'Downloads', 'instantclient_19_8')
-# cx_Oracle.init_oracle_client(lib_dir=lib_dir)
-# connection = cx_Oracle.connect('tip', 'AaZZ0r_cle#1', 'iberiadb_medium')
-# cursor = connection.cursor()
+# Oracle Client config
+os.environ['TNS_ADMIN'] = '/Users/elenaborisova/Documents/GitHub/iberia-solution/wallet'
+lib_dir = os.path.join(os.environ.get('HOME'), 'Downloads', 'instantclient_19_8')
+cx_Oracle.init_oracle_client(lib_dir=lib_dir)
+connection = cx_Oracle.connect('tip', 'AaZZ0r_cle#1', 'iberiadb_medium')
+cursor = connection.cursor()
 
 
 @app.route('/')
@@ -35,24 +33,23 @@ def index():
 
 @app.route('/register/', defaults={'msg': ' '})
 @app.route('/register/<msg>', methods=['GET'])
-# @app.route('/register', methods=['GET'])
 def register(msg):
     return render_template('register.html', msg=msg)
 
 
 @app.route('/register', methods=['POST'])
 def handle_register():
-    # username = request.form['username']
-    # email = request.form['email']
-    # password = request.form['password']
-    # hashed_password = generate_password_hash(password)
-    #
-    # insert_query = f'''
-    #             INSERT INTO registered_users(username, email, password)
-    #             VALUES ('{username}', '{email}', '{hashed_password}')
-    #             '''
-    # cursor.execute(insert_query)
-    # connection.commit()
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    hashed_password = generate_password_hash(password)
+
+    insert_query = f'''
+                INSERT INTO registered_users(username, email, password)
+                VALUES ('{username}', '{email}', '{hashed_password}')
+                '''
+    cursor.execute(insert_query)
+    connection.commit()
 
     return redirect(url_for('register', msg='Thank you for registering! Your credentials are being reviewed!'))
 
@@ -67,34 +64,27 @@ def handle_login():
     username = request.form['username']
     password = request.form['password']
 
-    if username == 'elena' and password == 'elena':
-        session.permanent = True
+    login_query = f'''
+    SELECT user_id, password
+    FROM authorized_users
+    WHERE username='{username}'
+    '''
+    cursor.execute(login_query)
+    connection.commit()
+    user = cursor.fetchone()
+
+    if user and check_password_hash(user[1], password):
+        session['user_id'] = user[1]
         session['username'] = username
         return redirect(url_for('index'))
     else:
         return render_template('403.html'), 403
 
-    # login_query = f'''
-    # SELECT user_id, password
-    # FROM authorized_users
-    # WHERE username='{username}'
-    # '''
-    # cursor.execute(login_query)
-    # connection.commit()
-    # user = cursor.fetchone()
-    #
-    # if user and check_password_hash(user[1], password):
-    #     session['user_id'] = user[1]
-    #     session['username'] = username
-    #     return redirect(url_for('index'))
-    # else:
-    #     return render_template('403.html'), 403
-
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    # session.pop('user_id')
+    session.pop('username')
+    session.pop('user_id')
 
     return redirect(url_for('index'))
 
@@ -102,7 +92,7 @@ def logout():
 @app.route('/upload/', defaults={'msg': ' '})
 @app.route('/upload/<msg>')
 def upload_file(msg):
-    if 'username' not in session:
+    if 'user_id' not in session:
         return render_template('403.html'), 403
     return render_template('upload.html', msg=msg)
 
@@ -123,13 +113,14 @@ def handle_upload():
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            modify_uploaded_file(filename)
+            cleaned_filename = modify_uploaded_file(filename)
+            upload_to_db(cleaned_filename)
             return redirect(url_for('upload_file', msg='File uploaded successfully!'))
 
 
 @app.route('/dash')
 def dashboard():
-    if 'username' not in session:
+    if 'user_id' not in session:
         return render_template('403.html'), 403
 
     data = {
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sqlalchemy as sa
 
 
 def allowed_file(filename, ALLOWED_EXTENSIONS):
@@ -13,3 +14,11 @@ def modify_uploaded_file(filename):
     read_file.to_csv(f'./file_uploads/{clean_filename}.csv', index=None, header=False)
     os.remove(f'./file_uploads/{filename}')
     return f'{clean_filename}.csv'
+
+
+def upload_to_db(filename):
+    data = pd.read_csv(f'./file_uploads/{filename}')
+    oracle_db = sa.create_engine('oracle+cx_oracle://tip:AaZZ0r_cle#1@iberiadb_medium')
+    connection = oracle_db.connect()
+    data.to_sql('monthly_incidents_raised', con=connection, if_exists='append', chunksize=1000, index=False)
+    print("Record inserted successfully")
